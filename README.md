63f99df1c9e560318bb35d1893e2bbc563426862
Andrew Luca <thendrluca@gmail.com>
2017-05-07 14:21:41 +0300

    Added raw-loader

---
+++ README.md
@@ -0,0 +1,1 @@
+## TMD Syntax Highlighter
--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-// ==UserScript==
+/*! ==UserScript==
 // @name        TMD BB Code Highilighter
 // @description This script will add a feauture to torrentsmd.com post menu. Use old code button, 20th in the row.
 // @include     /^http(s)?:\/\/(www\.)?torrentsmd\.(com|me|eu)\/forum\.php\?action=(viewforum|viewtopic).+/
-// @resource    codemirror_css https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.25.2/codemirror.min.css
-// @require     https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.25.2/codemirror.min.js
-// @grant       GM_addStyle
-// @grant       GM_getResourceText
 // @copyright   2013+, trolic, Spider
-// ==/UserScript==
+// ==/UserScript== */
+
+import css from 'raw-loader!./styles.css';
+import html from 'raw-loader!./markup.html';
+import languages from 'json-loader!./languages.json';
 
 (function ($) {
 
-    // INJECT STYLES
-    GM_addStyle(GM_getResourceText('codemirror_css'));
 
-    // INJECT HTML
-    $('<div class="jora">').appendTo(document.body);
 
     $(function () {
 
         // REMOVE OLD LISTENERS
         // ADD NEW LISTENERS
 
-        console.log(666);
+        setTimeout(alert, 5000);
 
     });
 })(jQuery);
